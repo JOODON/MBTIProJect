05395c4d36f75399abcb2d4d4420a7b4cf2ce624
BoYoung00 <qhdud20@naver.com>
2022-11-01 10:56:33 +0900

    bb
    dd

--- src/main/java/Service/MemberService.java
+++ src/main/java/Service/MemberService.java
@@ -7,6 +7,9 @@ import java.util.List;
 public interface MemberService {
     public MemberDto addUser(MemberDto memberDto);
 
+<<<<<<< Updated upstream
     public MemberDto login(MemberDto memberDto) throws Exception;
+=======
+>>>>>>> Stashed changes
 
 }
